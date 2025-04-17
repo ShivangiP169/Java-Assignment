2d7a8772db69c1a460c49c7279f9805a48423e3c
ShivangiP169 <gp1293134@gmail.com>
2025-04-17 03:02:59 -0400

    Add files via upload

---
+++ File_Open.java
@@ -0,0 +1,13 @@
+package Sample;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class File_Open {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
